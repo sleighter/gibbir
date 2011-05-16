b268c0f2f042eb55788ee0e725e1e227acb18cf5
Alex Sleighter <alex@Derringer.(none)>
2011-05-16 13:28:33 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GIBBIR::Application.config.secret_token = '2b9ca3060e2a894b63259d4577f098a2d9317541ece07804bac31fa8a7d249e7b2c3487caf49da1ce983aca368b64842262ca939c4c23831e979f81dee9e1dbe'
